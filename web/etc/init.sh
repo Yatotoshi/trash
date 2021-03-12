b818dadd606884777fa4071592b5731ef1849c37
Misha Yatotoshi <liannosoul@gmail.com>
2021-03-12 14:30:30 +0300

    third

--- web/etc/init.sh
+++ web/etc/init.sh
@@ -1,4 +1,4 @@
 gunicorn --bind='0.0.0.0:8080' hello:test &
-python ../../ask/manage.py runserver 0.0.0.0:8081 &
+python ../ask/manage.py runserver 0.0.0.0:8081 &
 sudo nginx -c /home/box/nginx.conf;
 echo "runing..."
